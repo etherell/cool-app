acd6edbc82eb80adbceb9a172cf303de690be5d9
Kostia <kostyaetherell@gmail.com>
2020-04-08 10:27:19 +0300

    adding avatar

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,6 @@
 class UsersController < ApplicationController
+	before_action :logged_in_user, only: [:edit, :update]
+
 	# Показ странички юзера
 	def show
 	    @user = User.find(params[:id])
@@ -11,21 +13,44 @@ class UsersController < ApplicationController
 	end
 
 	def create
-	    @user = User.new(user_params)    # Не окончательная реализация!
+	    @user = User.new(user_params) 
 	    if @user.save
-	       flash[:success] = "Welcome to the Sample App!"
-      		redirect_to @user
+	       flash[:success] = "Please log in"
+      		redirect_to login_path
 	    else
 	      	render 'new'
 	    end
 	end
+
+
+	def edit
+    	@user = User.find(params[:id])
+  	end
+
+  	def update
+    	@user = User.find(params[:id])
+    	if @user.update_attributes(user_params)
+      		flash[:success] = "Profile updated"
+     		redirect_to @user
+    	else
+      		render 'edit'
+    	end
+  	end
 
 	private
 
 	# Приватный метод получающий параметры
 	def user_params
       params.require(:user).permit(:name, :email, :password,
-                                   :password_confirmation)
+                                   :password_confirmation, :avatar)
+    end
+
+    # Подтверждение входа пользователя
+    def logged_in_user
+        unless logged_in?
+          flash[:danger] = "Please log in."
+          redirect_to login_url
+        end
     end
 
 end
